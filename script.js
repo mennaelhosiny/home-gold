1c618cbdb4534526d7952eac06dbdc280b1c5270
mennaelhosiny <mennaelhosiny3@gmail.com>
2024-07-28 17:32:36 +0300

    updated

--- script.js
+++ script.js
@@ -101,4 +101,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+    
+
+
+  //lazy load
+  document.addEventListener('DOMContentLoaded', function() {
+    let lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+
+    if ('IntersectionObserver' in window) {
+        let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.classList.remove('lazy');
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(function(lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    } else {
+        lazyImages.forEach(function(lazyImage) {
+            lazyImage.src = lazyImage.dataset.src;
+        });
+    }
+});
+
   
